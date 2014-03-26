1d0b2f6aa1ec9364fba9f6276988643da9ea9d5b
Alexander Deider <for_inbox@mail.ru>
2014-03-27 02:05:17 +0700

    disallow changing Hasher::abc

--- Xiag/Utils/Hasher.php
+++ Xiag/Utils/Hasher.php
@@ -8,12 +8,7 @@ use Xiag\ISaveable;
 class Hasher
 {
   private $abc = "0123456789_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  public function setAlphabet($abc)
-  {
-    //abc should not contain duplicates;
-    $this->abc = $abc;
-  }
-
+  //adding setter or config var for alphabet invalidate existing data... need solve this before allow different abcs
   private function num2hash($num)
   {
     if ($num <= 0) return '';
@@ -34,7 +29,6 @@ class Hasher
     $indexes = array_flip(str_split($this->abc));
     $num = 0;
     $base = strlen($this->abc);
-    // if (count($indexes)!==$base) "Wrong alphabet :( There're duplicates";
     for ($i = 0,$len = strlen($hash); $i < $len; $i++) {
       if(!isset($indexes[$hash[$i]])) return false;
       $digit = $indexes[$hash[$i]];
